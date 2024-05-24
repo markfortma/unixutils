a4ef5ddf829f88f210a319e98c181dd323a55484
Matthew Markfort <matthew.markfort@gmail.com>
2024-05-24 17:22:48 -0500

    working wgrep

--- wgrep.c
+++ wgrep.c
@@ -10,7 +10,72 @@
 
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
+#include<pcre.h>
+
+#define CAPTUREVECTORSIZE 30
+
+void usage(const char *progname){
+  printf("Usage: %s Regexp file1 file2 ...\n", progname);
+}
+
+int match_in_file(const char *path, pcre *regexp){
+  char *line = NULL;
+  int lineno = 0;
+  size_t linelen = 0;
+  ssize_t nbytes = 0;
+  FILE *in = NULL;
+
+  int rc;
+  int capturegroups[CAPTUREVECTORSIZE];
+  /* open file for reading*/
+  in = fopen(path, "r");
+  while(!feof(in)){
+    /* read a line from in */
+    nbytes = getline(&line, &linelen, in);
+    /* if 0 bytes were read, at end of file - exit while */
+    if(0 == nbytes) break;
+
+    rc = pcre_exec(
+		   regexp,        // compile regex
+		   NULL,          // optional results from pcre_study
+		   line,          // input string
+		   (int) linelen, // input string length
+		   0,             // start of string offset
+		   0,             // PCRE flags
+		   capturegroups,
+		   CAPTUREVECTORSIZE);
+    // handle errors from pcre_exec
+    if(0 <= rc)
+      printf("Line %i: %s\n", lineno, line);
+    lineno++;
+  }
+  /* free the buffer containing line */
+  if(line != NULL) free(line);
+  fclose(in);
+}
 
 int main(int argc, char *argv[]){
+  if(argc < 3){
+    usage(argv[0]);
+    return EXIT_FAILURE;
+  }
+  int errcode = 0;
+  const char *errmsg = NULL;
+  pcre *regexp = pcre_compile(argv[1], // RegExp pattern
+			      PCRE_CASELESS,       // flags
+			      &errmsg, // error message
+			      &errcode,// error code
+			      NULL);   // use default locale
+  if(regexp == NULL){
+    fprintf(stderr, "failed to compile RegExp: %d: %s\n", errcode, errmsg);
+    return EXIT_FAILURE;
+  }
+
+  /* cycle through files and output their matching lines */
+  for(int i = 2; i < argc; i++)
+    match_in_file(argv[i], regexp);
+  /* free the regexp pattern */
+  pcre_free(regexp);
   return EXIT_SUCCESS;
 }
